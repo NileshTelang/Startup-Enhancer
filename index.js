64241cc0cd054d55c196caab4963e22fb790dc00
Nova <nileshtelang07n5@gmail.com>
2023-03-06 13:30:34 +0530

    local authentication completed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//jshint esversion:6
 
 require('dotenv').config();
 
@@ -43,7 +44,7 @@ app.use(passport.session());
 mongoose.connect("mongodb://127.0.0.1:27017/userDB"); //localhost ain't working => 127.0.0.1
 
 const userSchema = new mongoose.Schema ({
-  email: String,
+  email : String,
   password: String,
   googleId: String,
   sTitle: String,
@@ -61,16 +62,16 @@ passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => done(null, user))
 });
+
+
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:4131/auth/google/secrets",
+    callbackURL: "http://localhost:4131/auth/google/home",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -83,22 +84,22 @@ passport.use(new GoogleStrategy({
 ));
 
 app.get("/", function(req, res){
-  res.render("home");
+  res.render("opening");
 });
 
 app.get("/auth/google",
   passport.authenticate('google', { scope: ["profile"] })
 );
 
-app.get("/auth/google/startups",
-  passport.authenticate('google', { failureRedirect: "/login" }),
+app.get("/auth/google/home",
+  passport.authenticate('google', { failureRedirect: "/opening" }),
   function(req, res) {
     // Successful authentication, redirect to secrets.
     res.render("home");
   });
 
 app.get("/register", function(req, res){
-  res.sendFile(__dirname+"/register.html");
+  res.render("register");
 });
 
 app.get("/startup", function(req, res){
@@ -113,61 +114,53 @@ app.get("/startup", function(req, res){
   });
 });
 
-// app.get("/submit", function(req, res){
-//   if (req.isAuthenticated()){
-//     res.render("submit");
-//   } else {
-//     res.redirect("/login");
-//   }
-// });
-
-// app.post("/submit", function(req, res){
-//   const submittedTitle = req.body.sTitle;
-//   const submittedImage = req.body.sImage;
-
-// // Once the user is authenticated and their session gets saved, their user details are saved to req.user.
-// // console.log(req.user.id);
-
-//   User.findById(req.user.id, function(err, foundUser){
-//     if (err) {
+app.get("/home", async (req, res) => {
+  try {
+    const founduser = await User.find({});
+    res.render("home");
+  } catch (err) {
+    console.log(err);
+  }
+});
+// app.get("/home", function (req, res)  {
+//   User.find({"username": {$ne: null}}, function(err, foundUsers){
+//     if (err){
 //       console.log(err);
 //     } else {
-//       if (foundUser) {
-//         foundUser.sTitle = submittedTitle;
-//         foundUser.sImage = submittedImage;
-//         foundUser.save(function(){
-//           res.redirect("/startup");
-//         });
+//       if (foundUsers) {
+//         res.render("home", {usersWithSecrets: foundUsers});
 //       }
 //     }
 //   });
 // });
 
-// app.get("/logout", function(req, res){
-//   req.logout(function(err){
-//     if(err){
-//       console.log(err);
-//     }else{
-//       console.log("Session LOgged Out")
-//     }
-//   });
-//   res.redirect("/");
-// });
 
-// app.post("/register", function(req, res){
+app.get("/logout", function(req, res){
+  req.logout(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("Session LOgged Out")
+    }
+  });
+  res.redirect("/");
+});
 
-//   User.register({username: req.body.username}, req.body.password, function(err, user){
-//     if (err) {
-//       console.log(err);
-//       res.redirect("/register");
-//     } else {
-//       passport.authenticate("local")(req, res, function(){
-//         res.redirect("/secrets");
-//       });
-//     }
-//   });
+app.post("/register", function(req, res){
 
-// });
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/home");
+      });
+    }
+  });
+
+});
+
 
 app.post("/opening", function(req, res){
 
@@ -190,120 +183,24 @@ app.post("/opening", function(req, res){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //mainpageabout
-// app.get("/about",function(req,res){
-//   res.sendFile(__dirname+"/about.html");
-// });
-
-
-
-// // startups
-
-// homeStartingContent = "roronoa";
-
-// let posts = [];
-
-
-
-// app.get("/startups", function(req, res){
-//   res.render("startup", {
-//     startingContent: homeStartingContent,
-//     posts: posts
-//     });
-// });
-
-
-// app.post("/startup", function(req, res){
-//   const post = {
-//     title: req.body.postTitle,
-//     content: req.body.postBody
-
-//   };
-
-//   posts.push(post);
-
-//   res.sendFile(__dirname+"/index.html");
-
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.get("/startup/:sTitle", function(req, res){
-//   const requestedTitle = _.lowerCase(req.params.sTitle);
-
-//   posts.forEach(function(post){
-//     const storedTitle = _.lowerCase(post.title);
-
-//     if (storedTitle === requestedTitle) {
-//       res.render("post", {
-//         title: post.title,
-//         content: post.content
-//       });
-//     }
-//   });
-
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //contactContent
 app.get("/contact", function(req, res){
-  res.sendFile(__dirname+"contact.html");
+  res.render("contact");
 });
 
 // hr-finance-buiseness
 
 app.get("/hr",function(req,res){
-  res.sendFile(__dirname+"/hr.html");
+  res.render("hr");
 });
 
 app.get("/buiseness",function(req,res){
-  res.sendFile(__dirname+"/buiseness.html");
+  res.render("buiseness");
 });
 
 
 app.get("/finance",function(req,res){
-  res.sendFile(__dirname+"/finance.html");
+  res.render("finance");
 });
 
 
