eb0f09d7eaeba8036181bf018b6746f2a4b0512b
Nova <nileshtelang07n5@gmail.com>
2023-03-08 17:00:14 +0530

    conditional rendering from database done

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
+const { url } = require('inspector');
 
 
 const app = express();
@@ -25,7 +26,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './Views'));
 
 //bodyParser use
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //express access public dir
 app.use(express.static(path.join(__dirname, 'public')));
@@ -42,14 +43,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.connect("mongodb://127.0.0.1:27017/fTeamDB"); //localhost ain't working => 127.0.0.1
-
-const userSchema = new mongoose.Schema ({
-  email : String,
+const db = mongoose.connection;
+const userSchema = new mongoose.Schema({
+  email: String,
   password: String,
-  googleId: String,
-  sTitle: String,
-  // sImage: String
+  googleId: String
 });
+
+
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
@@ -58,7 +59,7 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
@@ -69,12 +70,12 @@ passport.deserializeUser((id, done) => {
 
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:4131/auth/google/home",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-  },
-  function(accessToken, refreshToken, profile, cb) {
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:4131/auth/google/home",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+  function (accessToken, refreshToken, profile, cb) {
     console.log(profile);
 
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
@@ -83,8 +84,24 @@ passport.use(new GoogleStrategy({
   }
 ));
 
-app.get("/", function(req, res){
-  res.render("home");
+// startups
+// create an schema
+var startupSchema = new mongoose.Schema({
+  email: String,
+  title: String,
+  description: String,
+  technologies: String,
+  category: String,
+  img: String,
+});
+
+var sups = mongoose.model('startups', startupSchema);
+
+
+app.get("/", async (req, res) => {
+  const docs = await sups.find({});
+  res.render("home",{startups : docs });
+
 });
 
 app.get("/auth/google",
@@ -93,22 +110,22 @@ app.get("/auth/google",
 
 app.get("/auth/google/home",
   passport.authenticate('google', { failureRedirect: "/opening" }),
-  function(req, res) {
+  function (req, res) {
     // Successful authentication, redirect to secrets.
     res.render("home");
   });
 
-app.get("/register", function(req, res){
+app.get("/register", function (req, res) {
   res.render("register");
 });
 
-app.get("/startup", function(req, res){
-  User.find({"secret": {$ne: null}}, function(err, foundUsers){
-    if (err){
+app.get("/startup", function (req, res) {
+  User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
+    if (err) {
       console.log(err);
     } else {
       if (foundUsers) {
-        res.render("startup", {usersWithSecrets: foundUsers});
+        res.render("startup", { usersWithSecrets: foundUsers });
       }
     }
   });
@@ -117,32 +134,32 @@ app.get("/startup", function(req, res){
 app.get("/home", async (req, res) => {
   try {
     const founduser = await User.find({});
-    res.render("home");
+    res.render("home", { startups: null });
   } catch (err) {
     console.log(err);
   }
 });
 
 
-app.get("/logout", function(req, res){
-  req.logout(function(err){
-    if(err){
+app.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       console.log("Session LOgged Out")
     }
   });
   res.redirect("/");
 });
 
-app.post("/register", function(req, res){
+app.post("/register", function (req, res) {
 
-  User.register({username: req.body.username}, req.body.password, function(err, user){
+  User.register({ username: req.body.username }, req.body.password, function (err, user) {
     if (err) {
       console.log(err);
       res.redirect("/register");
     } else {
-      passport.authenticate("local")(req, res, function(){
+      passport.authenticate("local")(req, res, function () {
         res.redirect("/home");
       });
     }
@@ -151,18 +168,18 @@ app.post("/register", function(req, res){
 });
 
 
-app.post("/opening", function(req, res){
+app.post("/opening", function (req, res) {
 
   const user = new User({
     username: req.body.username,
     password: req.body.password
   });
 
-  req.login(user, function(err){
+  req.login(user, function (err) {
     if (err) {
       console.log(err);
     } else {
-      passport.authenticate("local")(req, res, function(){
+      passport.authenticate("local")(req, res, function () {
         res.redirect("/home");
       });
     }
@@ -170,30 +187,66 @@ app.post("/opening", function(req, res){
 
 });
 
+app.get("/submit", function (req, res) {
+  res.render("submit");
+})
+
+app.post("/submit", async (req, res)=> {
+
+
+  const email = req.body.email;
+  const title = req.body.sTitle;
+  const description = req.body.description;
+  const technologies = req.body.technologies;
+  const category = req.body.category;
+  const img = req.body.image;
+
+
+  var data = {
+    "email": email,
+    "title": title,
+    "description": description,
+    "technologies": technologies,
+    "category": category,
+    "img": img,
+  }
+
+  db.collection('startups').insertOne(data, async (err, collection) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("Record Inserted Successfully!!")
+      const docs = await sups.find({});
+      res.render("home",{startups : docs });
+    }
+
+  });
+
+});
 
 
 //contactContent
-app.get("/contact", function(req, res){
+app.get("/contact", function (req, res) {
   res.render("contact");
 });
 
 // hr-finance-buiseness
 
-app.get("/hr",function(req,res){
+app.get("/hr", function (req, res) {
   res.render("hr");
 });
 
-app.get("/buisness",function(req,res){
+app.get("/buisness", function (req, res) {
   res.render("buisness");
 });
 
 
-app.get("/finance",function(req,res){
+app.get("/finance", function (req, res) {
   res.render("finance");
 });
 
 
 //listen method
-app.listen(process.env.PORT || 4131 , function() {
+app.listen(process.env.PORT || 4131, function () {
   console.log("Server started on port 4131");
 });
